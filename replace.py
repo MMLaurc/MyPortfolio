6efa9d65445d5b8c654f027e115fcd9f48a2fc82
Marian Mihaila <151676189+MMLaurc@users.noreply.github.com>
2024-03-31 19:14:56 +0100

    Add files via upload
    This first time working with strings

---
+++ replace.py
@@ -0,0 +1,18 @@
+"""
+create variable call sentence and store the text
+create variable call sentence_replace using .replace function to replace "!" and add space " "
+print first sentence replaceing ! and adding space
+print sentence again keeping replace and adding .upper
+print sentence reversed using [::-1]
+
+"""
+sentence ="The!quick!brown!fox!jumps!over!the!lazy!dog."
+sentence_replace = sentence.replace("!"," ")
+print(sentence_replace)
+print(sentence_replace.upper())
+print(sentence_replace[::-1])
+
+
+#when you will check my work please let me know on your review how is better to do
+#writing pseudocode with """ """ or #
+#thank you!
---
+++ strings.py
@@ -0,0 +1,6 @@
+# I will create a variable call here and the string value "$$$Superman$$$"
+# I will add .strip function for strings to remove '$' from front and end of Superman
+# after that print the result
+
+hero = "$$$Superman$$$"
+print(hero.strip('$'))
