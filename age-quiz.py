22a4d860fb3f3e72e5944640dcc5d3f0c2992ebd
Marian Mihaila <151676189+MMLaurc@users.noreply.github.com>
2024-03-31 19:15:54 +0100

    Add files via upload
    Starting to work with if, elif, else

---
+++ age-quiz.py
@@ -0,0 +1,7 @@
+
+print("Example 6: ")
+item = "chais"
+if item == "dog" or len(item) == 5:                 #Branch 1
+        print("Either item is a dog, or the len of your item is 5.")
+elif item == "chair" and len(item) == 5:             #Branch 2
+       print("Your item is both a chair and len of item is 5.")
